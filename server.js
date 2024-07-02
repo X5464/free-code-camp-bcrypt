d12dd9cfeaeb664f97f2dd51e0065b741e569855
X5464 <rajarshi7474@gmail.com>
2024-07-03 04:44:50 +0530

    update

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.get('/', (req, res) => {
   });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err,hash) => {
+  return hash
+});
+bcrypt.compare(myPlaintextPassword,hash, (err,res) => {
+  console.log(res)
+});
 
 
 //END_ASYNC
